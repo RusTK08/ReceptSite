fd6568d319bf12d4e5dacb5051b1cbf38c40cec0
Ruslan Romanov <ruslan97_19972mail.ru>
2025-02-14 17:43:35 +0300

    sdfewgf

--- receptsite/receptsite/urls.py
+++ receptsite/receptsite/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('recept/', include('recept.urls')),
-    # path('myauth/' include('myauth.urls')),
+    path('myauth/', include('myauth.urls')),
 ]
